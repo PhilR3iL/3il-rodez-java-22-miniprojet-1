ccc750c957326a4c58e88b27b397cc95f73a0f87
LuluValette <valetteluc14@gmail.com>
2023-12-14 15:55:44 +0100

    Modification du code des fichiers Bruit2D et BruitAleatoire

--- src/fr/ecole3il/rodez2023/perlin/math/BruitAleatoire.java
+++ src/fr/ecole3il/rodez2023/perlin/math/BruitAleatoire.java
@@ -1,13 +1,19 @@
 package fr.ecole3il.rodez2023.perlin.math;
 
 import java.util.Random;
+import java.util.random.RandomGenerator;
 
 /**
  * La classe RandomNoise étend la classe Noise2D et génère du bruit aléatoire en deux dimensions.
  * Elle utilise la classe Random de Java pour générer des valeurs aléatoires.
  */
 public class BruitAleatoire extends Bruit2D {
+    public BruitAleatoire(long graine, double resolution) {
+        super(graine, resolution);
+    }
 
-    public BruitAleatoire(long graine, double resolution);
-    
+    @Override
+    public double bruit2D(double x, double y) {
+        return RandomGenerator.getDefault().nextDouble(x, y);
+    }
 }
