2b5c6a348bf8888446daf83aa9d361c180f0fa93
LuluValette <valetteluc14@gmail.com>
2024-02-21 01:12:21 +0100

    Génération de la carte terminée

---
+++ src/fr/ecole3il/rodez2023/perlin/exception/TerrainInexistant.java
@@ -0,0 +1,7 @@
+package fr.ecole3il.rodez2023.perlin.exception;
+
+public class TerrainInexistant extends IndexOutOfBoundsException {
+    public TerrainInexistant(String s) {
+        super(s);
+    }
+}
