f63b956a0d354fb8b5747cd66f2a682709a73abb
LuluValette <valetteluc14@gmail.com>
2024-02-22 21:59:00 +0100

    commit avant changement de branche

--- src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurPerlin.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurPerlin.java
@@ -4,33 +4,32 @@ import fr.ecole3il.rodez2023.perlin.math.BruitPerlin2D;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.Terrain;
 
 public class GenerateurPerlin extends GenerateurCarte{
+    BruitPerlin2D bruitPerlinHydrometrie;
+    BruitPerlin2D bruitPerlinTemperature;
+    BruitPerlin2D bruitPerlinAltitude;
+    BruitPerlin2D bruitPerlinDetails;
+    BruitPerlin2D bruitPerlinForms;
 
     public GenerateurPerlin(long graine) {
         super(graine);
-        // TODO Auto-generated constructor stub
+        long graineDetails = graine*3;
+        long graineForms = graine*4;
+        bruitPerlinHydrometrie = new BruitPerlin2D(graine,1);
+        bruitPerlinTemperature = new BruitPerlin2D(graine*2,1);
+        bruitPerlinAltitude = new BruitPerlin2D(graine*4,1);
+        bruitPerlinForms = new BruitPerlin2D(graineForms,1);
+        bruitPerlinDetails = new BruitPerlin2D(graineDetails,1);
     }
 
     @Override
     protected Terrain genererTerrain(int i, int j, int largeur, int hauteur) {
-        /**Generation des valeurs de types BruitPerlin2D
-         *
-         * Vous êtes parti très très très loin dans la réalisation de cette classe
-         * J'ai failli faire un coma
-         *
-         * @BruitPerlin2D --> génération des valeurs avec des graines*/
-        BruitPerlin2D altitudePerlin = new BruitPerlin2D(getGraine(), 1);
-        BruitPerlin2D hydrometriePerlin = new BruitPerlin2D(getGraine(), 1);
-        BruitPerlin2D temperaturePerlin = new BruitPerlin2D(getGraine() * 2, 1);
-        /**@double x/y --> création des coordonnées*/
-        double x = (double) i / (double) largeur;
-        double y = (double) j / (double) hauteur;
-        /**@double --> attribution des valeurs aux attributs*/
-        double altitude = altitudePerlin.bruit2D(x, y);
-        double hydrometrie = hydrometriePerlin.bruit2D(x, y);
-        double temperature = temperaturePerlin.bruit2D(x, y);
-        hydrometrie = (hydrometrie + 1) / 2;
-        temperature = (temperature + 1) / 2 ;
-        /**@return --> création d'un nouveau terrain*/
+        double x = (double)i / largeur;
+        double y = (double)j / hauteur;
+
+        double altitude = bruitPerlinAltitude.bruit2D(x, y);
+        double hydrometrie = Math.abs(bruitPerlinHydrometrie.bruit2D(x, y));
+        double temperature = Math.abs(bruitPerlinTemperature.bruit2D(x, y));
+
         return new Terrain(altitude, hydrometrie, temperature);
     }
 
--- src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/VisualiseurTerrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/VisualiseurTerrain.java
@@ -5,7 +5,7 @@ import fr.ecole3il.rodez2023.perlin.terrain.carte.Carte;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.Terrain;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.TypeTerrain;
 
-public class VisualiseurTerrain {
+public abstract class VisualiseurTerrain {
     private DetermineurTerrain determineurTerrain;
     private Carte carte;
     public VisualiseurTerrain(Carte carte, DetermineurTerrain determineurTerrain){
